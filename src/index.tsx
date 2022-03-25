57754a3af54afa1c062e3c378cbdb23d2a5887d1
ademidun <tomiademidun@gmail.com>
2022-03-24 20:59:29 -0400

    support both popup and dashboard view:
    see: https://meerkat-citronella.github.io/jekyll/update/2020/10/01/welcome-to-jekyll.html#:~:text=How%20is%20this%20working%3F%20Well%2C%20when%20we%20build%20the%20React%20app%2C%20all%20these%20files%20are%20going%20to%20be%20in%20the%20build%20directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import PopUp from './scenes/PopUp/PopUp';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const popupRoot = document.getElementById("popup-root");
+
+// Main Extensions page
+!popupRoot &&
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+
+// PopupComponent / popup.html
+popupRoot &&
+  ReactDOM.render(
+    <React.Fragment>
+      <PopUp />
+    </React.Fragment>,
+    popupRoot
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
