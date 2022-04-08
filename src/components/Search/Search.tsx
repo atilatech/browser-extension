e09aaf8e32dc22aa358009599635a407714fbaae
ademidun <tomiademidun@gmail.com>
2022-04-08 18:47:40 -0400

    move ContentCard into it's own component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,7 @@
 import { ChangeEventHandler, KeyboardEventHandler, useState } from 'react';
 import { Content } from '../../models/Content';
 import AtlasAPI from '../../services/AtlasAPI';
-import { TextUtils } from '../../services/utils/TextUtils';
+import { ContentCard } from '../ContentCard/ContentCard';
 
 function Search() {
 
@@ -18,7 +18,6 @@ function Search() {
           event.preventDefault();
           AtlasAPI.search(searchQuery)
                 .then(res=> {
-                    console.log({res});
                     const { results } = res;
                     setSearchResults(results);
                 })
@@ -32,37 +31,16 @@ function Search() {
             <input value={searchQuery} className="form-control mb-3" onChange={updateSearch} onKeyDown={keyDownHandler} 
             name="searchQuery" placeholder="Enter Search Term" />
 
-            {searchResults && 
+            {searchResults.length > 0 && 
                 <>
                 <hr/>
-                {searchResults.map(searchResult => <SearchResult content={searchResult} />)}
+                {searchResults.map(searchResult => <ContentCard content={searchResult} />)}
                 </>
             }
         </div>
     )
 }
 
-export interface SearchResultProps {
-    content: Content;
-};
 
-export function SearchResult(props: SearchResultProps) {
-
-  const { content } = props;
-
-  return (
-    <div className="card shadow m-3 p-3">
-      <a className="text-align-left" href={content.url} target="_blank" rel="noopener noreferrer">
-          {content.title} <br/>
-          {
-          content.header_image_url && 
-          <img src={content.header_image_url} width="150"
-          alt={content.title} />
-          }
-      </a>
-      <p>{TextUtils.truncate(content.description, 140)} </p>
-    </div>
-  )
-}
 
 export default Search
