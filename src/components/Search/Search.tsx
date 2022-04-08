bfbf996bc07121540690defaf5f8aea1f8ac3ebf
ademidun <tomiademidun@gmail.com>
2022-04-08 09:03:49 -0400

    display search results

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,7 @@
 import { ChangeEventHandler, KeyboardEventHandler, useState } from 'react';
 import { Content } from '../../models/Content';
 import AtlasAPI from '../../services/AtlasAPI';
+import { TextUtils } from '../../services/utils/TextUtils';
 
 function Search() {
 
@@ -18,6 +19,8 @@ function Search() {
           AtlasAPI.search(searchQuery)
                 .then(res=> {
                     console.log({res});
+                    const { results } = res;
+                    setSearchResults(results);
                 })
                 .catch(err => {
                     console.log({err});
@@ -28,8 +31,38 @@ function Search() {
         <div className="Search">
             <input value={searchQuery} className="form-control mb-3" onChange={updateSearch} onKeyDown={keyDownHandler} 
             name="searchQuery" placeholder="Enter Search Term" />
+
+            {searchResults && 
+                <>
+                <hr/>
+                {searchResults.map(searchResult => <SearchResult content={searchResult} />)}
+                </>
+            }
         </div>
     )
+}
+
+export interface SearchResultProps {
+    content: Content;
+};
+
+export function SearchResult(props: SearchResultProps) {
+
+  const { content } = props;
+
+  return (
+    <div className="card shadow m-3 p-3">
+      <a className="text-align-left" href={content.url} target="_blank" rel="noopener noreferrer">
+          {content.title} <br/>
+          {
+          content.header_image_url && 
+          <img src={content.header_image_url} width="150"
+          alt={content.title} />
+          }
+      </a>
+      <p>{TextUtils.truncate(content.description, 140)} </p>
+    </div>
+  )
 }
 
 export default Search
