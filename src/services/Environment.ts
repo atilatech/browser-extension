b4df885276899ce59b494d13ea8ab5ba6e1c143d
Tomiwa Ademidun <oademidun@wealthsimple.com>
2022-06-07 20:02:38 -0400

    add staging environment and change logic for determining environment

--- src/services/Environment.ts
+++ src/services/Environment.ts
@@ -12,10 +12,22 @@ export const EnvironmentProd = {
     clientUrl: "https://atila.ca",
 };
 
+
+export const EnvironmentStaging = {
+    apiUrl: "https://atila-7-staging.herokuapp.com",
+    clientUrl: "https://staging.atila.ca",
+};
+
 // set to EnvironmentDev as the default so we can use type hinting and the autocomplete feature
 let Environment = EnvironmentDev;
 
 if(process.env.REACT_APP_ATILA_STAGE === "prod"){
+    Environment =  EnvironmentProd;
+}
+
+if (window.location.host === 'staging.atlas.atila.ca') {
+    Environment = EnvironmentStaging;
+} else if (window.location.host === 'atlas.atila.ca') {
     Environment =  EnvironmentProd;
 }
 
